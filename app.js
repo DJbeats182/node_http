38eaa1a6f5be36b7801679e5dd883d7240088c37
maul49zer@gmail.com <maul49zer@gmail.com>
2023-06-06 13:37:04 -0600

    done

--- app.js
+++ app.js
@@ -1,8 +1,72 @@
-console.log("Hello World!\n==========\n");
-
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
-
+const { error } = require("console");
 const http = require("http");
 
-// Finish setting up the server
+const populateHTML = (headingContent) => {
+  return `<main style="background-color: burlywood; height: 100vh; margin: 0; display: flex; justify-content: center; align-items: center;">
+    <h1>${headingContent}</h1>
+    </main>`;
+};
+
+http
+  .createServer((request, response) => {
+    const { url, method } = request;
+    const chunks = [];
+
+    request
+      .on("error", (error) => {
+        response.statusCode = 400;
+        response.setHeader("Content-Type", "application/json");
+        response.write(JSON.stringify(error));
+        response.end();
+      })
+      .on("data", (chunk) => {
+        chunks.push(chunk);
+      })
+      .on("end", () => {
+        console.log(chunks);
+
+        const body = Buffer.concat(chunks).toString();
+        const responseBody = {
+          url,
+          method,
+          body,
+        };
+
+        response.on("error", (error) => {
+          response.statusCode = 500;
+          response.setHeader("Content-Type", "application/json");
+          response.write(JSON.stringify(error));
+          response.end();
+        });
+
+        switch (url) {
+          case "/":
+            response.setHeader("Content-Type", "text/html");
+            response.write(populateHTML("Welcome to my Server, Enjoy!"));
+            break;
+          case "/about":
+            const details = {
+              name: "DJ",
+              city: "Vineyard",
+            };
+
+            response.setHeader("Content-Type", "application/json");
+            response.write(JSON.stringify(details));
+            break;
+          case "/echo":
+            response.setHeader("Content-Type", "application/json");
+            response.write(JSON.stringify(responseBody));
+            break;
+          default:
+            response.setHeader("Content-Type", "text/html");
+            response.write(
+              populateHTML(
+                "404 Not Found. Try <a href='http://localhost:3000'>this</a>"
+              )
+            );
+        }
+
+        return response.end();
+      });
+  })
+  .listen(3000, () => console.log("Server listening on port 3000..."));
